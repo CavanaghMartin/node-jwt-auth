5d4a946c3bece19323ac56692272a016a9f03963
zaratush <martinc.1@hotmail.com>
2021-07-11 23:12:44 -0300

    try catch fix

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -35,11 +35,16 @@ export const getProfile = async (req, res) => {
   // res.status(200).send(decoded);
   // Search the Info base on the ID
   // const user = await User.findById(decoded.id, { password: 0});
-  const user = await User.findById(req.userId, { password: 0 });
-  if (!user) {
-    return res.status(404).send("No user found.");
+  try {
+
+    const user = await User.findById(req.userId, { password: 0 });
+    if (!user) {
+      return res.status(404).send("No user found.");
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error)
   }
-  res.status(200).json(user);
 };
 
 export const signinController = async (req, res) => {
--- api/src/libs/verifyToken.js
+++ api/src/libs/verifyToken.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import config from "../config";
+import User from "../models/User";
 
 export async function verifyToken(req, res, next) {
   // Get the token from the headers
@@ -11,12 +12,22 @@ export async function verifyToken(req, res, next) {
       .status(401)
       .send({ auth: false, message: "No Token aws Provided" });
   }
+  try {
 
-  // decode the token
-  const decoded = await jwt.verify(token, config.secret);
+    // decode the token
+    const decoded = await jwt.verify(token, config.secret);
+    console.log(decoded,"sdfsdfd")
+    // save the token on request object to using on routes
+    const user = await User.findById(decoded.id, { password: 0 });
 
-  // save the token on request object to using on routes
-  req.userId = decoded.id;
+    if (!user) {
+      return res.status(404).send("No user found.");
+    }
+    req.userId = decoded.id;
+  } catch (error) {
+    return res.status(400).send("jwt is incorrect");
+
+  }
 
   // continue with the next function
   next();
